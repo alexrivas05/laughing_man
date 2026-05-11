40c40d8ccf59216f41d9929e8f6ae858dc8f50df
alexrivas05 <alex12rivas34@gmail.com>
2026-05-10 20:49:33 -0400

    Added get color function

--- laughing_man.js
+++ laughing_man.js
@@ -47,3 +47,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
     });
 
 });
+
+function getColor(){
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
